d446e8a2a239c7849f7eb033ab346245305f3ab2
Mukesh K <me.mukesh18@gmail.com>
2020-08-11 00:34:40 +0530

    Added dataSize prog in Others

---
+++ Others/dataSize.c
@@ -0,0 +1,10 @@
+//program for finding the size of data types
+
+#include<stdio.h>
+
+void main(){
+	printf("The size of integer\t=\t %ld bytes.\n", sizeof(int));
+	printf("The size of float\t=\t %ld bytes.\n", sizeof(float));
+	printf("The size of character\t=\t %ld bytes.\n", sizeof(char));
+	printf("The size of double\t=\t %ld bytes.\n", sizeof(double));
+}
