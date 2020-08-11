71d5614011e245ddfc866b64e5a5988676499324
Mukesh K <me.mukesh18@gmail.com>
2020-08-12 00:31:52 +0530

    Added swapNums prog in Others

---
+++ Others/swapNums.c
@@ -0,0 +1,13 @@
+// Program for swapping of two numbers using third variable
+// compile using gcc -o swapNums swapNums.c (ubuntu)
+#include<stdio.h>
+
+void main(){
+	int num1, num2, temp;
+	printf("Enter the values for swapping [num1, num2]:\n");
+	scanf("%d %d", &num1, &num2);
+	temp = num1;
+	num1 = num2;
+	num2 = temp;
+	printf("The values after swapping is num1 = %d and num2 = %d\n", num1, num2);
+}
