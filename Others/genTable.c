9b7d42228006679236cf4109646048e91df0aa97
Mukesh K <me.mukesh18@gmail.com>
2020-08-16 22:15:03 +0530

    Added genTable prog in Others

---
+++ Others/genTable.c
@@ -0,0 +1,24 @@
+/* Program to generate the table of numbers entered by user */
+
+#include<stdio.h>
+
+int table(int, int);
+
+void main(){
+	int i, n;
+	printf("Enter any integer: ");
+	scanf("%d", &n);
+	for(i=1; i<=10; i++){
+		printf("%d x %d = %d\n", n, i, table(n, i));
+	}
+	
+	printf("\n");
+}
+
+int table(int n, int i){
+	if(i == 1){
+		return n;
+	}else{
+		return (table(n, i-1) + n);
+	}
+}
