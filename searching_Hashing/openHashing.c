99acf2637906165183874fa44b8055cfc0c3a3c5
Mukesh K <me.mukesh18@gmail.com>
2020-09-07 14:41:05 +0530

    Added openHashing.c to searching_Hashing

---
+++ searching_Hashing/openHashing.c
@@ -0,0 +1,117 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+#define size 7
+
+struct node{
+    int data;
+    struct node *next;
+};
+
+struct node *chain[size];
+
+void init(){
+    int i;
+    for(i = 0; i < size; i++)
+        chain[i] = NULL;
+}
+
+
+void insert(int value){
+	//create a newnode with value
+	struct node *newNode = malloc(sizeof(struct node));
+	newNode->data = value;
+	newNode->next = NULL;
+
+	//calculate hash key
+	int key = value % size;
+
+	//check if chain[key] is empty
+	if(chain[key] == NULL)
+		chain[key] = newNode;
+	//collision
+	else{
+		//add the node at the end of chain[key].
+		struct node *temp = chain[key];
+		while(temp->next){
+		    temp = temp->next;
+		}
+
+		temp->next = newNode;
+	}
+}
+
+int del(int value){
+    int key = value % size;
+    struct node *temp = chain[key], *dealloc;
+    if(temp != NULL){
+        if(temp->data == value){
+            dealloc = temp;
+            temp = temp->next;
+            free(dealloc);
+            return 1;
+        }
+        else{
+            while(temp->next){
+                if(temp->next->data == value){
+                    dealloc = temp->next;
+                    temp->next = temp->next->next;
+                    free(dealloc);
+                    return 1;
+                }
+                temp = temp->next;
+            }
+        }
+    }
+
+    return 0;
+}
+
+
+void print(){
+	int i;
+
+	for(i = 0; i < size; i++)	{
+		struct node *temp = chain[i];
+		printf("chain[%d]-->",i);
+		while(temp){
+			printf("%d -->",temp->data);
+			temp = temp->next;
+		}
+		printf("NULL\n");
+	}
+}
+
+
+void main(){
+    //init array of list to NULL
+	init();
+
+	insert(7);
+	insert(0);
+	insert(3);
+	insert(10);
+	insert(4);
+	insert(5);
+
+	print();
+	printf("\n");
+	
+	if(del(10)){
+        printf("After Deletion of 10\n");
+        print();
+     }
+    else
+        printf("Value Not Present\n");
+}
+
+
+
+
+
+
+
+
+
+
+
